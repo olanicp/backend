4f8ad953a0c9719e6ec4531606d036c69c533d14
olanicp <olanicpon22@gmail.com>
2025-01-16 18:15:32 +0100

    change streak logic + add fetching name from db

--- server.js
+++ server.js
@@ -11,10 +11,18 @@ const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 app.post("/register", async (req, res) => {
-  const { email, password } = req.body;
+  const { name, email, password } = req.body;
 
   try {
-    const { data, error } = await supabase.auth.signUp({ email, password });
+    const { data: registerData, error } = await supabase.auth.signUp({
+      email,
+      password,
+    });
+
+    if (userError) {
+      console.error("Error fetching user data:", userError.message);
+      return res.status(400).json({ error: "Could not retrieve user data." });
+    }
 
     if (error) {
       console.error("Error registering user:", error.message);
@@ -22,9 +30,12 @@ app.post("/register", async (req, res) => {
     }
 
     if (data) {
+      const userId = registerData.user.id;
       const { error: dataError } = await supabase
         .from("user_data")
-        .insert([{ id: data.user.id, streak_count: 0 }]);
+        .insert([
+          { id: userId, streak_count: 0, login_days: [], userName: name },
+        ]);
 
       if (dataError) {
         console.error("Error creating user data:", dataError);
@@ -148,7 +159,6 @@ app.get("/stats", async (req, res) => {
 
 app.post("/login", async (req, res) => {
   const { email, password } = req.body;
-
   try {
     const { data, error } = await supabase.auth.signInWithPassword({
       email,
@@ -160,10 +170,68 @@ app.post("/login", async (req, res) => {
       return res.status(400).json({ error: error.message });
     }
     const userId = data.user.id;
+
     const { data: userData, error: userError } = await supabase
       .from("user_data")
-      .select("streak_count, login_days")
+      .select("user_name")
       .eq("id", userId)
+      .single();
+
+    if (userError) {
+      console.error("Error while trying to fetch name in: ", userError.message);
+      return res.status(400).json({ error: userError.message });
+    }
+
+    return res.status(200).json({
+      message: "User logged in successfully",
+      user: {
+        userID: userId,
+        email: data.user.email,
+        name: userData.user_name,
+      },
+    });
+  } catch (err) {
+    console.error("Unexpected error:", err);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.get("/getStreak", async (req, res) => {
+  const { userID } = req.query;
+
+  try {
+    const { data: userData, error: userError } = await supabase
+      .from("user_data")
+      .select("streak_count, login_days, lastInterviewedAt")
+      .eq("id", userID)
+      .single();
+
+    if (userError) {
+      console.error("Error fetching user data:", userError.message);
+      return res.status(400).json({ error: "Could not retrieve user data." });
+    }
+
+    return res.status(200).json({
+      message: "Data download successfull",
+      streakData: {
+        streak: userData.streak_count,
+        loginDays: userData.login_days,
+        interviewedAt: userData.lastInterviewedAt,
+      },
+    });
+  } catch (err) {
+    console.error("Unexpected error:", err);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.post("/updateStreak", async (req, res) => {
+  const { userID } = req.body;
+  try {
+    const { data: userData, error: userError } = await supabase
+      .from("user_data_extended")
+      .select("*")
+      .eq("id", userID)
       .single();
 
     if (userError) {
@@ -173,32 +241,49 @@ app.post("/login", async (req, res) => {
 
     const now = new Date();
     const currentDayIndex = (now.getDay() + 6) % 7;
-    const lastSignedAt = new Date(data.user.last_sign_in_at);
+
+    const lastSignedAt = new Date(userData.last_sign_in_at);
+    let lastInterviewedAt = new Date(userData.lastInterviewedAt);
     let newStreakCount = userData.streak_count;
     let newLoginDays = [...(userData.login_days || [])];
-    const timeDifference = now.getTime() - lastSignedAt.getTime();
-    const hoursDifference = timeDifference / (1000 * 60 * 60);
-    const createdAt = new Date(data.user.created_at);
-    if (createdAt.getDay() === now.getDay()) {
-      //TODO: change for the date (lastsignedAt) from database
-      newStreakCount = 1;
-      newLoginDays = [currentDayIndex];
-      console.log(createdAt.getDay(), lastSignedAt.getDay(), "what1");
-    } else if (hoursDifference > 24) {
-      newStreakCount = 1;
-      newLoginDays = [currentDayIndex];
-      console.log("what2");
-    } else if (hoursDifference <= 24 && hoursDifference > 0) {
-      newStreakCount += 1;
-      if (!newLoginDays.includes(currentDayIndex)) {
-        newLoginDays.push(currentDayIndex);
-      }
-      console.log("what3");
-    } else {
-      newStreakCount = 1;
-      console.log("what4");
-    }
+    const createdAt = new Date(userData.created_at);
 
+    const calculateDayDifference = (date1, date2) => {
+      const startOfDate1 = new Date(
+        date1.getFullYear(),
+        date1.getMonth(),
+        date1.getDate()
+      );
+      const startOfDate2 = new Date(
+        date2.getFullYear(),
+        date2.getMonth(),
+        date2.getDate()
+      );
+      return Math.floor((startOfDate2 - startOfDate1) / (1000 * 60 * 60 * 24));
+    };
+
+    if (lastInterviewedAt == null) {
+      newStreakCount = 1;
+      newLoginDays = [currentDayIndex];
+      lastInterviewedAt = now;
+    } else {
+      const dayDifference = calculateDayDifference(lastInterviewedAt, now);
+      if (dayDifference > 1) {
+        newStreakCount = 1;
+        newLoginDays = [currentDayIndex];
+        lastInterviewedAt = now;
+      } else if (dayDifference === 1) {
+        if (!newLoginDays.includes(currentDayIndex)) {
+          newStreakCount += 1;
+          newLoginDays.push(currentDayIndex);
+          lastInterviewedAt = now;
+        }
+      } else {
+        newStreakCount = 1;
+        newLoginDays = [currentDayIndex];
+        lastInterviewedAt = now;
+      }
+    }
     if (newLoginDays.length > 7) {
       newLoginDays = [currentDayIndex];
     }
@@ -208,8 +293,9 @@ app.post("/login", async (req, res) => {
       .update({
         streak_count: newStreakCount,
         login_days: newLoginDays,
+        lastInterviewedAt: lastInterviewedAt,
       })
-      .eq("id", userId);
+      .eq("id", userID);
 
     if (updateError) {
       console.error("Error updating user streak:", updateError.message);
@@ -217,17 +303,117 @@ app.post("/login", async (req, res) => {
     }
 
     return res.status(200).json({
-      message: "User logged in successfully",
-      user: {
-        userID: userId,
-        email: data.user.email,
-        streak: newStreakCount,
+      message: "Data updated successfully",
+      updatedData: {
         login_days: newLoginDays,
+        lastInterviewed: lastInterviewedAt,
+        streak: newStreakCount,
       },
     });
   } catch (err) {
     console.error("Unexpected error:", err);
     return res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.post("/saveUserInterview", async (req, res) => {
+  const { emotionsIDs, quadrant, activities, userID } = req.body;
+  try {
+    const { data: interviewData, error } = await supabase
+      .from("user_interview")
+      .insert([
+        {
+          user_id: userID,
+          emotion_ids: emotionsIDs,
+          date: new Date(),
+          quadrant: quadrant,
+          sleepingHours: activities.sleepingHours,
+          exerciseHours: activities.exerciseHours,
+          meals: activities.meals,
+          activities: activities.activities,
+        },
+      ]);
+
+    if (error) {
+      console.error("Błąd podczas wstawiania danych:", error);
+      return res
+        .status(500)
+        .json({ message: "Nie udało się zapisać danych", error });
+    }
+
+    res.status(200).json({ message: "Dane zostały zapisane pomyślnie" });
+  } catch (err) {
+    console.error("Nieoczekiwany błąd:", err);
+    res.status(500).json({ message: "Wewnętrzny błąd serwera" });
+  }
+});
+
+app.get("/emotions", async (req, res) => {
+  const { x, y } = req.query;
+  const newX = parseFloat((x - 8) / 8);
+  const newY = parseFloat((y - 8) / 8);
+
+  if (newX === undefined || newY === undefined) {
+    return res.status(400).send({ error: "Brak współrzędnych x i y" });
+  }
+
+  try {
+    const { data: emotions, error } = await supabase
+      .from("emotions")
+      .select("*")
+      .gte("pleasantness", newX - 0.25)
+      .lte("pleasantness", newX + 0.25)
+      .gte("energy", newY - 0.25)
+      .lte("energy", newY + 0.25);
+
+    if (error) {
+      throw error;
+    }
+
+    let quadrant = "";
+    if (newX >= 0 && newY >= 0) quadrant = "high energy pleasant";
+    else if (newX >= 0 && newY < 0) quadrant = "low energy pleasant";
+    else if (newX < 0 && newY >= 0) quadrant = "high energy unpleasant";
+    else quadrant = "low energy unpleasant";
+
+    res.send({ emotions, quadrant });
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+});
+
+app.get("/emotionsByIds", async (req, res) => {
+  const { IDs } = req.query;
+  try {
+    const { data: emotionsByIds, error } = await supabase
+      .from("emotions")
+      .select("*")
+      .in("id", IDs);
+
+    if (error) {
+      throw error;
+    }
+    res.send({ emotionsByIds });
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+});
+
+app.get("/stats", async (req, res) => {
+  const { userID } = req.query;
+  try {
+    const { data: stats, error } = await supabase
+      .from("user_interview")
+      .select("*")
+      .eq("user_id", userID);
+
+    if (error) {
+      throw error;
+    }
+
+    res.send({ stats });
+  } catch (err) {
+    res.status(500).send({ error: err.message });
   }
 });
 
